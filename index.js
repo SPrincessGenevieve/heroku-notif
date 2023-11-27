29b11b8a61b885a96ed075bded197e230ee56f5f
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-27 19:34:05 +0800

    Remove unwanted lockfile

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
-
+const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+const io = new Server(server);
+
+// Adjust the CORS configuration for production
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with the actual URL of your React app
+  methods: ["GET", "POST"],
+};
+
+app.use(cors(corsOptions));
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -22,6 +23,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
 });
