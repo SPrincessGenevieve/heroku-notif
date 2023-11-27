5759c93fe97a33368847e419a2b50ce276ba20fe
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-11-27 22:39:20 +0800

    socket.io starting point

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 const express = require("express");
+const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
-const io = new Server(server);
-
-// Adjust the CORS configuration for production
-const corsOptions = {
-  origin: "http://localhost:3000", // Replace with the actual URL of your React app
-  methods: ["GET", "POST"],
-};
-
-app.use(cors(corsOptions));
-
-// Set up a simple CSP allowing unsafe inline scripts (for development only)
-const csp = {
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-inline'"],
-    // Add other directives as needed
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
-};
-
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", getCSPHeader(csp));
-  next();
 });
 
 io.on("connection", (socket) => {
@@ -37,16 +22,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
-
-server.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
 });
-
-// Function to generate CSP header
-function getCSPHeader(csp) {
-  const header = Object.entries(csp.directives)
-    .map(([directive, sources]) => `${directive} ${sources.join(" ")}`)
-    .join("; ");
-  return header;
-}
